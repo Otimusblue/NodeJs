249b367cb7e05d310d43ef2f2b0eddb37a314761
Macintosh HD <otimusblue@gmail.com>
2018-11-19 21:07:16 +0700

    Local-persist

--- nodejs-demo1/node-persist.js
+++ nodejs-demo1/node-persist.js
@@ -4,43 +4,84 @@ var student = {
   id : "",
   fullname: ""
 }
-var options = {
-  dir : "mydata",
-  ttl : false
-}
-storage.initSync(options);
+
+storage.initSync({
+  dir : "students"
+});
 
 //storage.setItemSync('sub','1');
 
 function getAllStudent()
 {
-   var students = storage.getItemSync('student');
+   var students = storage.getItemSync('students');
+   var matchedStudent = null;
    console.log(students);
-   if (typeof students === "underfined"){
+   if (typeof students === "undefined"){
        return [];
    }
    return students;
 }
 
-function showStudents(){
+function getStudent(studentId){
+
   var students = getAllStudent();
-  for (var i = 0; i < students.length;i++){
-    console.log('Student: ',students[i].fullname + "(" + students[i].id + ")");
+  var matchedStudent = null;
+
+  for(var i = 0; i < students.length; i++){
+    if (students[i].id === studentId){
+      matchedStudent = students[i];
+      break;
+    }
   }
+  return matchedStudent;
 }
 
 
 function addStudent(studentId, studentName){
-  var students = getAllStudent();
+ var students = getAllStudent();
   students.push({
     id : studentId,
     fullname : studentName
   });
+   
   storage.setItemSync('students',students);
 }
 
-addStudent(1,'Hieu');
-addStudent(2,'Huyen');
-addStudent(3,'Nhan');
+function removeStudent(studentId){
+  var students = getAllStudent();
+
+  for(var i = 0; i < students.length; i++){
+    if(students[i].id === studentId){
+      students.splice(i,1);
+    }
+  }
+  storage.setItemSync('students',students);
+}
+
+function editStudent(studentId, studentName){
+
+  var students = getAllStudent();
+  for (var i = 0; i < students.length; i++){
+    if (students[i].id === studentId){
+      students[i].fullname = studentName;
+    }
+  }
+}
+
+function showStudents(){
+  var students = getAllStudent();
+  // for (var i = 0; i < students.length;i++){
+  //    console.log('Student: ',students[i].fullname + "(" + students[i].id + ")");
+  // }
+  students.forEach(function(student){
+    console.log('Student : ' + student.fullname + '(' + student.id + ')');
+  });
+}
+
+
+
+addStudent(1, 'Hieu');
+addStudent(2, 'Huyen');
+addStudent(3, 'Nhan');
 
 showStudents();
